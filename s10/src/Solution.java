e0443cfde1f00d3596d99b814453ed18f9165353
yztz <781089956@qq.com>
2021-07-14 16:11:10 +0800

    s10 c2

--- s10/src/Solution.java
+++ s10/src/Solution.java
@@ -1,10 +1,10 @@
 import java.util.Arrays;
 
-/*https://leetcode-cn.com/problems/minimum-absolute-sum-difference/ */
+/*https://leetcode-cn.com/problems/minimum-absolute-sum-difference/ 63ms */
 public class Solution {
     public static void main(String[] args) {
-        int[] num1 = {94,43,100,2,53,31,73,82,70,94};
-        int[] num2 = {80,91,1,62,13,37,86,53,43,49};
+        int[] num1 = {5,2,7};
+        int[] num2 = {10,7,12};
         Solution solution = new Solution();
         System.out.println(solution.minAbsoluteSumDiff(num1, num2));
     }
@@ -25,39 +25,47 @@ public class Solution {
         int idx = -1, val = Integer.MIN_VALUE;
         for (int i = 0; i < len; i++) {
             int tmp = nums2[i];
-            int delta1 = -Integer.MAX_VALUE;
-//            // 边界检查
-//            if (nums1[len - 1] - tmp >= 0) {
-//                int delta = nums1[len - 1] - tmp;
-//                int optimization = nums3[i] - delta;
-//                if (optimization > val) {
-//                    val = optimization;
-//                    idx = i;
-//                }
-//                continue;
-//            }
-//            int left = 0, right = len - 2;
-//            while (left <= right) {
-//                int mid = (left + right) / 2;
-//                int delta2 = nums1[mid] - tmp;
-//                if ( )
-//            }
-
-            for (int num : nums1) {
-                int delta2 = num - tmp;
-                if (delta2 >= 0) {
-//                    System.out.println("delta1 = " + delta1 + " delta2 = " + delta2);
+            // 边界检查
+            if (nums1[len - 1] - tmp <= 0) {
+                int delta = nums1[len - 1] - tmp;
+                int optimization = nums3[i] + delta;
+                if (optimization > 0 && optimization > val) {
+                    val = optimization;
+                    idx = i;
+                }
+                continue;
+            }
+            if (nums1[0] - tmp >= 0) {
+                int delta = nums1[0] - tmp;
+                int optimization = nums3[i] - delta;
+                if (optimization > 0 && optimization > val) {
+                    val = optimization;
+                    idx = i;
+                }
+                continue;
+            }
+            int left = 0, right = len - 1;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                int delta1 = nums1[mid] - tmp;
+                int delta2 = nums1[mid + 1] - tmp;
+                if (delta1 > 0) {
+                    right = mid;
+                } else if (delta2 < 0) {
+                    left = mid;
+                } else {
+                    System.out.println("i = " + i + " delta1 = " + delta1 + " delta2 = " + delta2);
                     int delta = Math.min(-delta1, delta2);
                     int optimization = nums3[i] - delta;
 //                    System.out.println("opt = " + optimization);
-                    if (optimization > val) {
+                    if (optimization > 0 && optimization > val) {
                         val = optimization;
                         idx = i;
                     }
                     break;
                 }
-                delta1 = delta2;
             }
+
         }
 //        System.out.println("idx = " + idx + " opt = " + val);
         int ans = 0;
@@ -66,9 +74,10 @@ public class Solution {
                 ans += nums3[i] - val;
             else
                 ans += nums3[i];
+            ans %= (int) (Math.pow(10, 9) + 7);
         }
 
 
-        return ans % (int)(Math.pow(10, 9) + 7);
+        return ans;
     }
 }

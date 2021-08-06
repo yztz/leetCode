9a71582dc1d512cfaaf3a3e52977ef38ed020b8a
yztz <781089956@qq.com>
2021-08-06 23:45:47 +0800

    s33

---
+++ s33/src/Solution.java
@@ -0,0 +1,8 @@
+
+/* https://leetcode-cn.com/problems/shortest-path-visiting-all-nodes/ */
+public class Solution {
+
+    public int shortestPathLength(int[][] graph) {
+        return -1;
+    }
+}

cd8ecd8070f1efc14af31ed11fe34377ad570e87
781089956 <781089956@qq.com>
2021-08-19 22:00:00 +0800

    s46

---
+++ s46/src/Solution.java
@@ -0,0 +1,33 @@
+/* https://leetcode-cn.com/problems/reverse-vowels-of-a-string/ 太简单了 直接cv*/
+public class Solution {
+    public String reverseVowels(String s) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        int i = 0, j = n - 1;
+        while (i < j) {
+            while (i < n && !isVowel(arr[i])) {
+                ++i;
+            }
+            while (j > 0 && !isVowel(arr[j])) {
+                --j;
+            }
+            if (i < j) {
+                swap(arr, i, j);
+                ++i;
+                --j;
+            }
+        }
+        return new String(arr);
+    }
+
+    public boolean isVowel(char ch) { // 短匹配值得学习
+        return "aeiouAEIOU".indexOf(ch) >= 0;
+    }
+
+    public void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+

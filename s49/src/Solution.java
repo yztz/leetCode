aeb0ec1cf01b6432d9bb5172a8944836ae0f8f49
781089956 <781089956@qq.com>
2021-08-22 22:43:01 +0800

    s49

---
+++ s49/src/Solution.java
@@ -0,0 +1,19 @@
+
+/* https://leetcode-cn.com/problems/escape-the-ghosts/solution/tao-tuo-zu-ai-zhe-by-leetcode-solution-gjga/ 新概念：曼哈顿距离 */
+public class Solution {
+    public boolean escapeGhosts(int[][] ghosts, int[] target) {
+        int[] source = {0, 0};
+        int distance = manhattanDistance(source, target);
+        for (int[] ghost : ghosts) {
+            int ghostDistance = manhattanDistance(ghost, target);
+            if (ghostDistance <= distance) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int manhattanDistance(int[] point1, int[] point2) {
+        return Math.abs(point1[0] - point2[0]) + Math.abs(point1[1] - point2[1]);
+    }
+}

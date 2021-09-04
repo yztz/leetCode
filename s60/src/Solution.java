f91fb30b7d3009111790472d3776ea66838a9d16
aikhi007 <781089956@qq.com>
2021-09-04 23:56:03 +0800

    s60

---
+++ s60/src/Solution.java
@@ -0,0 +1,35 @@
+/* https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/ 形势所迫-.- */
+public class Solution {
+    static final int MOD = 1000000007;
+
+    public int fib(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int[][] q = {{1, 1}, {1, 0}};
+        int[][] res = pow(q, n - 1);
+        return res[0][0];
+    }
+
+    public int[][] pow(int[][] a, int n) {
+        int[][] ret = {{1, 0}, {0, 1}};
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                ret = multiply(ret, a);
+            }
+            n >>= 1;
+            a = multiply(a, a);
+        }
+        return ret;
+    }
+
+    public int[][] multiply(int[][] a, int[][] b) {
+        int[][] c = new int[2][2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                c[i][j] = (int) (((long) a[i][0] * b[0][j] + (long) a[i][1] * b[1][j]) % MOD);
+            }
+        }
+        return c;
+    }
+}

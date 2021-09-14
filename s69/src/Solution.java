b12046c44463e2a1be5c192d2ab69ad452ff429b
aikhi007 <781089956@qq.com>
2021-09-14 23:52:49 +0800

    s69

---
+++ s69/src/Solution.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.List;
+
+/* https://leetcode-cn.com/problems/longest-word-in-dictionary-through-deleting/submissions/ */
+class Solution {
+    public String findLongestWord(String s, List<String> dictionary) {
+        int m = s.length();
+        int[][] f = new int[m + 1][26];
+        Arrays.fill(f[m], m);
+
+        for (int i = m - 1; i >= 0; --i) {
+            for (int j = 0; j < 26; ++j) {
+                if (s.charAt(i) == (char) ('a' + j)) {
+                    f[i][j] = i;
+                } else {
+                    f[i][j] = f[i + 1][j];
+                }
+            }
+        }
+        String res = "";
+        for (String t : dictionary) {
+            boolean match = true;
+            int j = 0;
+            for (int i = 0; i < t.length(); ++i) {
+                if (f[j][t.charAt(i) - 'a'] == m) {
+                    match = false;
+                    break;
+                }
+                j = f[j][t.charAt(i) - 'a'] + 1;
+            }
+            if (match) {
+                if (t.length() > res.length() ||  (t.length() == res.length() && t.compareTo(res) < 0)) {
+                    res = t;
+                }
+            }
+        }
+        return res;
+    }
+}

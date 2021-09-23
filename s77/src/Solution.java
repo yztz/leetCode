af622b35491a3b8260896c2f7e70ed7ea9c4d1f1
aikhi007 <781089956@qq.com>
2021-09-23 12:56:17 +0800

    s77☆

---
+++ s77/src/Solution.java
@@ -0,0 +1,7 @@
+/* https://leetcode-cn.com/problems/power-of-three/  */
+public class Solution {
+
+    public boolean isPowerOfThree(int n) {
+        return n > 0 && 1162261467 % n == 0;
+    }
+}

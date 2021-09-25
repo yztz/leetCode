e147568842c34b44c804a9acdbd0f51f8ec8ac7e
aikhi007 <781089956@qq.com>
2021-09-25 10:55:49 +0800

    s80,81☆☆☆

---
+++ s80/src/Solution.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/* https://leetcode-cn.com/problems/delete-operation-for-two-strings/  */
+public class Solution {
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            char c1 = text1.charAt(i - 1);
+
+            for (int j = 1; j <= m; j++) {
+                char c2 = text2.charAt(j - 1);
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
